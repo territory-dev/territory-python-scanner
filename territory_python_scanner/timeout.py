ef7abff3c0fee2020127ab7804fa166ebbd81bd8
Paweł Stiasny <pawelstiasny@gmail.com>
2025-03-12 02:25:13 +0100

    dont spam timeout setup errors

--- territory_python_scanner/timeout.py
+++ territory_python_scanner/timeout.py
@@ -1,14 +1,22 @@
+setup_fail_logged = False
+
+
 def raise_te(*a):
     raise TimeoutError
 
 
 def setup_timeout(t):
+    global setup_fail_logged
+
     try:
         from signal import SIGALRM, alarm, signal
 
         signal(SIGALRM, raise_te)
         alarm(t)
     except Exception:
+        if setup_fail_logged:
+            return
+        setup_fail_logged = True
         print('failed to setup timeout')
 
 
